2669db2ef0f1050e0335afa52b50d45a17f7119f
Elvil <Elvil@users.noreply.github.com>
2015-05-26 20:43:37 +0200

    Génération des getter setter

--- EuroSim/src/main/sarl/fr/utbm/vi51/project/eurock/environment/MobileObject.java
+++ EuroSim/src/main/sarl/fr/utbm/vi51/project/eurock/environment/MobileObject.java
@@ -5,6 +5,46 @@ import fr.utbm.vi51.project.eurock.general.Vector2f;
 
 public class MobileObject extends WorldObject {
 
+	public Vector2f getCurrentLinearSpeed() {
+		return currentLinearSpeed;
+	}
+
+	public void setCurrentLinearSpeed(Vector2f currentLinearSpeed) {
+		this.currentLinearSpeed = currentLinearSpeed;
+	}
+
+	public Vector2f getCurrentAngularSpeed() {
+		return currentAngularSpeed;
+	}
+
+	public void setCurrentAngularSpeed(Vector2f currentAngularSpeed) {
+		this.currentAngularSpeed = currentAngularSpeed;
+	}
+
+	public float getMaxLinearSpeed() {
+		return maxLinearSpeed;
+	}
+
+	public void setMaxLinearSpeed(float maxLinearSpeed) {
+		this.maxLinearSpeed = maxLinearSpeed;
+	}
+
+	public float getMaxAngularSpeed() {
+		return maxAngularSpeed;
+	}
+
+	public void setMaxAngularSpeed(float maxAngularSpeed) {
+		this.maxAngularSpeed = maxAngularSpeed;
+	}
+
+	public float getMaxAcceleration() {
+		return maxAcceleration;
+	}
+
+	public void setMaxAcceleration(float maxAcceleration) {
+		this.maxAcceleration = maxAcceleration;
+	}
+
 	private Vector2f currentLinearSpeed;
 
 	private Vector2f currentAngularSpeed;
