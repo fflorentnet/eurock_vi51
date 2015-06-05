a2432f6bd17b3a00050c2ec3f14791090e8cca03
redhawk25 <nicolasfarine@me.com>
2015-06-06 01:48:46 +0200

    Ajout du ArrivingBehaviour

--- EuroSim/src/main/sarl/fr/utbm/vi51/project/eurock/behaviour2/ArrivingBehaviour.java
+++ EuroSim/src/main/sarl/fr/utbm/vi51/project/eurock/behaviour2/ArrivingBehaviour.java
@@ -2,6 +2,7 @@ package fr.utbm.vi51.project.eurock.behaviour2;
 
 import fr.utbm.info.vi51.framework.agent.BehaviourOutput;
 import fr.utbm.info.vi51.framework.math.Point2f;
+import fr.utbm.info.vi51.framework.math.Vector2f;
 
 /**
  * Arriving Behaviour.
@@ -11,6 +12,6 @@ import fr.utbm.info.vi51.framework.math.Point2f;
 
 public interface ArrivingBehaviour {
 
-	public BehaviourOutput runArriving(Point2f position, float linearSpeed, float maxLinearAcc, Point2f target);
+	public BehaviourOutput runArriving(Point2f position, Vector2f velocity, Point2f target, float maxLinearAcc, float maxLinearSpeed, float targetRadius, float slowRadius);
 	
 }
